8c02a4857683ab6ffdca446e3edc082cd9eb0ad4
cjstanzhu <cjstanzhu@gmail.com>
2019-11-19 22:42:08 -0500

    insert build

---
+++ client/build/precache-manifest.3715be35618dac993ad7e26057f4f97a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1e676d23aaff36f2e84a",
+    "url": "/static/js/main.32949ec3.chunk.js"
+  },
+  {
+    "revision": "aeb6a79592f12f3891a4",
+    "url": "/static/js/2.9a744969.chunk.js"
+  },
+  {
+    "revision": "3d9689d66a10178c2d612e325776dee7",
+    "url": "/index.html"
+  }
+];
